5e9e6f9e806031bb0ed236490fcbc1e0cc877f01
Hieu Manh <hieuluis96@gmail.com>
2019-12-03 09:25:06 +0700

    Updates

---
+++ precache-manifest.00579b9e2e018c0e81ed070c319f6d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2426b18cd9127acb5d12cdc5c9017b",
+    "url": "/HPEventWeb/index.html"
+  },
+  {
+    "revision": "198b8fcf2597db24a5fb",
+    "url": "/HPEventWeb/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b4fb75d83f9e44d3073a",
+    "url": "/HPEventWeb/static/js/2.f483b78d.chunk.js"
+  },
+  {
+    "revision": "198b8fcf2597db24a5fb",
+    "url": "/HPEventWeb/static/js/main.b1f54d2b.chunk.js"
+  },
+  {
+    "revision": "035cfc5ed0c9748b79b2",
+    "url": "/HPEventWeb/static/js/runtime-main.6666997b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/HPEventWeb/static/media/logo.25bf045c.svg"
+  }
+]);
